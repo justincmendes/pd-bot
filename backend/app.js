552ad72fc56e67bd8aa52fbcc05225bd5b52aa11
Justin Mendes <justincmendes@gmail.com>
2020-07-29 16:08:08 -0400

    Initalize express and backend files

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3002;
+const routes = require('./routes');
+
+app.use('/api', routes);
+
+app.listen(PORT, () => console.log(`Running on Port ${PORT}`
+));
