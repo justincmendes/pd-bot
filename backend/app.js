70794eb9c73fa383831a7fd74f87bb23eb96758f
Justin Mendes <justincmendes@gmail.com>
2020-08-18 00:25:49 -0400

    Get User (identify and guilds) Information from (discord) OAuth2

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,16 @@ const passport = require('passport');
 const app = express();
 const PORT = process.env.PORT || 3002;
 const routes = require('./routes');
+const mongoose = require("mongoose");
+const mongo = require("../utils/mongoose");
 
-app.use('/api', routes);
+mongo.init(true);
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use('/api', routes);
+
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
